95eea92b531a0f02ad7f1b6fd35807d014d24ee8
Robert Bray <rbray77777@gmail.com>
2025-06-10 15:24:41 -0400

    I'm currently still working on the game loop. must complete comments and check for edge cases.

--- war/src/GameLoop.java
+++ war/src/GameLoop.java
@@ -1,4 +1,21 @@
+import java.util.HashMap;
+import java.util.Stack;
+
 public class GameLoop {
+
+    public static Stack<Card> playPile = new Stack<>();
+    private static HashMap<String, Integer> getFaceRanks(){
+        HashMap<String, Integer> faceRanks = new HashMap<>();
+
+        faceRanks.put("Joker", 1);
+        faceRanks.put("King", 2);
+        faceRanks.put("Queen", 3);
+        faceRanks.put("Jack", 4);
+
+        return faceRanks;
+    }
+
+
 
     //intialize and shuffle deck
     //while loop for game play in main method
@@ -36,35 +53,98 @@ public class GameLoop {
         }
     }
 
-    public Player whoWonRound(Player playerOne, Player playerTwo){
-        Card playerOneCard = playerOne.placeCardIntoPlayPile();
-        Card playerTwoCard = playerTwo.placeCardIntoPlayPile();
+    // DOUBLE CHECk THIS FOR EDGE CASES, ESPECIALLY ADDING MORE HAND CARDS
+    private static Player warLogic(Player playerOne, Player playerTwo){
+
+        while(true){
+            for (int i = 0; i < 4; i++) {
+                playPile.push(playerOne.placeCardIntoPlayPile());
+                playPile.push(playerTwo.placeCardIntoPlayPile());
+
+            }
+            Card playerOneRevaledCard = playerOne.placeCardIntoPlayPile();
+            Card playerTwoRevaledCard = playerTwo.placeCardIntoPlayPile();
+            playPile.push(playerOneRevaledCard);
+            playPile.push(playerTwoRevaledCard);
+
+            if(whoWonRound(playerOne,playerTwo,playerOneRevaledCard, playerTwoRevaledCard) == playerOne){
+                return playerOne;
+            }  else if (whoWonRound(playerOne,playerTwo,playerOneRevaledCard, playerTwoRevaledCard) == playerTwo) {
+                return playerTwo;
+            }
+        }
+
+
+    }
+
+    // DOUBLE CHECK THIS FOR EDGE CASES
+    private static Player whoWonRound(Player playerOne, Player playerTwo, Card playerOneCard, Card playerTwoCard){
+
         if(playerTwoCard == null || playerOneCard == null){
             System.out.println("player has no more in hand cards. implement the out of hand card logic");
-            
+
         }
         if(isInteger(playerOneCard.getCardValue())&&isInteger(playerTwoCard.getCardValue())){
             int oneNum = Integer.parseInt(playerOneCard.getCardValue());
             int twoNum = Integer.parseInt(playerTwoCard.getCardValue());
-            if(oneNum>twoNum){
+            if(oneNum>twoNum && twoNum!=1){
                 return playerOne;
-            } else if (twoNum>oneNum) {
+            } else if (twoNum == 1 && oneNum != 1) {
                 return playerTwo;
+            } else if (twoNum>oneNum && oneNum!=1) {
+                return playerTwo;
+            } else if (oneNum ==1 && twoNum != 1) {
+                return playerOne;
             } else{
-                //war logic
+                return warLogic(playerOne, playerTwo);
             }
         } else if (isInteger(playerOneCard.getCardValue()) && isInteger(playerTwoCard.getCardValue()) == false) {
-            //make a method for face versus number
+            if(Integer.parseInt(playerOneCard.getCardValue()) == 1){
+                return playerOne;
+            } else {
+                return playerTwo;
+            }
         } else if(isInteger(playerOneCard.getCardValue()) == false && isInteger(playerTwoCard.getCardValue())){
-            //method for face versus number
+            if(Integer.parseInt(playerTwoCard.getCardValue())==1){
+                return playerTwo;
+            } else{
+                return playerOne;
+            }
+
         } else{
-            // face versus face logic.
+            if(getFaceRanks().get(playerOneCard.getCardValue()) > getFaceRanks().get(playerTwoCard.getCardValue())){
+                return playerOne;
+            } else if(getFaceRanks().get(playerTwoCard.getCardValue())> getFaceRanks().get(playerOneCard.getCardValue())){
+                return playerTwo;
+            } else {
+                return warLogic(playerOne,playerTwo);
+            }
+
         }
 
     }
 
-    public static void main(String[] args){
+    public static void redeemCards(Player winner){
+        while(!playPile.isEmpty()){
+            winner.populateCollectedCard(playPile.pop());
+        }
+    }
 
+    // MUST FINISH THIS
+    private static void populateHandCardsFromCollectedCards(Player player){
+        for (int i = 0; i < 4; i++) {
+            if(player.getCollectedCards().size() >= 4){
+                player.populateHandCard(player.getCollectedCards().pop());
+            } else {
+                // game over logic
+            }
+        }
+    }
+    
+    
+    // MUST FINISH THIS
+    public void playGame(){
+        boolean gameDone = false;
         GameLoop game = new GameLoop();
         Card[] gameCards = game.intializeAndShuffleDeck();
 
@@ -72,6 +152,48 @@ public class GameLoop {
         Player player_Two = new Player();
 
         game.doDeal(gameCards,player_One,player_Two);
+
+        while(gameDone == false){
+            if(player_One.getHandCards().size() == 0){
+                if(player_One.getCollectedCards().size() > 0){
+                    player_One.shuffleCollectedCard();
+                    populateHandCardsFromCollectedCards(player_One);
+
+                }
+            }
+            if(player_Two.getHandCards().size() == 0){
+                if(player_Two.getCollectedCards().size() > 0){
+                    player_Two.shuffleCollectedCard();
+                    populateHandCardsFromCollectedCards(player_Two);
+                }
+            }
+
+
+            Card playerOneCard = player_One.placeCardIntoPlayPile();
+
+            //a check for null and adding more cards if necssary
+            playPile.push(playerOneCard);
+
+            Card playerTwoCard = player_Two.placeCardIntoPlayPile();
+
+            //a check for null and adding more cards if necessary
+            playPile.push(playerTwoCard);
+
+            Player winner = whoWonRound(player_One,player_Two, playerOneCard, playerTwoCard);
+
+            if(winner!=null){
+                redeemCards(winner);
+            }
+
+            //if either player runs out of hand cards we populate their hand. by four
+
+
+        }
+
+    }
+
+    public static void main(String[] args){
+
 
 
     }
